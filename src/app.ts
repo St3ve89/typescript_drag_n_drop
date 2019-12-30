9b613dfdb3d7ab9be475b22742f8b0b6b1f4c1e1
Istvan Acs <istvan.acs89@gmail.com>
2019-12-30 17:23:09 +0000

    Rendering Project Items with a Class

--- src/app.ts
+++ src/app.ts
@@ -164,6 +164,26 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.ppl.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 // ProjectList class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -200,14 +220,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   }
 
   private renderProjects() {
+    console.log(this.element.id);
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
     listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
@@ -276,12 +295,10 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   @autobind
   private submitHandler(ev: Event) {
     ev.preventDefault();
-    console.log(this.titleInputEl.value);
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, ppl] = userInput;
       projectState.addProject(title, desc, ppl);
-      console.log(title, desc, ppl);
       this.clearInputs();
     }
   }
